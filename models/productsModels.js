9923649bc2773566263b58baa66cd1ba79d5f8c4
Christian Lessa <christianclessa@gmail.com>
2022-04-05 20:26:22 -0300

    Alterando connection na pasta Models

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,4 +1,4 @@
-const { connection } = require('./connection');
+const connection = require('./connection');
 
 const listAllProducts = async () => {
   const [products] = await connection.execute('SELECT * FROM StoreManager.products ORDER BY id;');
