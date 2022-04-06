0a81b64f4cb3a2192694ae80fe5cc5c5512ec6f1
Christian Lessa <christianclessa@gmail.com>
2022-04-06 15:42:05 -0300

    test sem validacoes products

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,14 +5,14 @@ const router = express.Router();
 const { 
   getAllProducts, getByIdProductCtrl, createProductCrtl, updateProductCrtl, deleteProductCrtl,
 } = require('../controllers/products');
-const { 
-  nameProductValid, quantityProductValid, createProductNameValid,
-} = require('../middlewares/productValidation');
+// const { 
+//   nameProductValid, quantityProductValid, createProductNameValid,
+// } = require('../middlewares/productValidation');
 
 router.get('/', getAllProducts);
 router.get('/:id', getByIdProductCtrl);
-router.post('/', nameProductValid, createProductNameValid, quantityProductValid, createProductCrtl);
-router.put('/:id', nameProductValid, quantityProductValid, updateProductCrtl);
+router.post('/', createProductCrtl);
+router.put('/:id', updateProductCrtl);
 router.delete('/:id', deleteProductCrtl);
 
 module.exports = router;
